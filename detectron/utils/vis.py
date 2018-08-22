fcafb5ae8cdc16de004151fcf19244179c7017e9
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-21 22:36:06 -0300

    Always outputs image

--- detectron/utils/vis.py
+++ detectron/utils/vis.py
@@ -251,8 +251,9 @@ def vis_one_image_opencv(
 def vis_one_image(
         im, im_name, output_dir, boxes, segms=None, keypoints=None, thresh=0.9,
         kp_thresh=2, dpi=200, box_alpha=0.0, dataset=None, show_class=False,
-        ext='pdf', out_when_no_box=False):
+        ext='pdf', **kwargs):
     """Visual debugging of detections."""
+    out_when_no_box = True
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
